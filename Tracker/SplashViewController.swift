a6c874feccac35e1691e4a2b584326d2b8ea96c1
CaptainSharky <chuikostepan@mail.ru>
2025-07-02 10:36:08 +0300

    build(Tracker): настроен LaunchScreen

---
+++ Tracker/SplashViewController.swift
@@ -0,0 +1,26 @@
+/*import UIKit
+
+final class SplashViewController: UIViewController {
+    private var logoImageView: UIImageView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+    }
+
+    private func setUI() {
+        view.backgroundColor = UIColor(resource: .ypBlue)
+
+        let logo = UIImage(resource: .logo)
+        let logoImageView = UIImageView(image: logo)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImageView)
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        self.logoImageView = logoImageView
+    }
+}
+*/
--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Valeriy Chuiko on 01.07.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let button = UIButton()
+
+        button.backgroundColor = .green
+        button.setTitle("Change color", for: .normal)
+        view.addSubview(button)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: button, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: .zero),
+            NSLayoutConstraint(item: button, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: .zero),
+            NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 50),
+            NSLayoutConstraint(item: button, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 200)
+        ])
     }
-
-
 }
 
